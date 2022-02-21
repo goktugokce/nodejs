d890bff710adbd68a8d5e9691258c6b922447524
goktugokce <goktugokce@windowslive.com>
2022-02-21 23:49:03 +0300

    chaining

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,16 +3,26 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 
-forecast(-75.7088, 44.1545, (error, data) => {
-  console.log('Error', error)
-  console.log('Data', data)
-})
+const address = process.argv[2];
+
+if(!address){
+  console.log("Please provide a city name");
+}
+else{
+  geocode(address, (error, data) => {
+    if(error){
+      return console.log(error);
+    }
+    forecast(data.latitude, data.lon, (error, forecastData) => {
+      if(error){
+        return console.log(error);
+      }
+      console.log(data.location)
+      console.log(forecastData)
+    })
+  });
+}
 
 
-// geocode('Philadelphia', (error, data) => {
-//   if(error){
-//     console.log('Error: ', error);
-//   }else{
-//     console.log('Data: ', data);
-//   }
-// });
+
+
--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
-  const url = 'http://api.weatherstack.com/current?access_key=179e304ba8eae4b428944234999d878b&query=' + longitude + ',' + latitude;
+  const url = 'http://api.weatherstack.com/current?access_key=179e304ba8eae4b428944234999d878b&query=' + latitude + ',' + longitude;
   request({url:url, json:true}, (error, response) => {
     if(error){
       callback('Unable to connect weather api', undefined);
@@ -10,11 +10,7 @@ const forecast = (latitude, longitude, callback) => {
       callback('Unable to find the location', undefined);
     }
     else{
-      callback(undefined, {
-        temperature:response.body.current.temperature,
-        precip:response.body.current.precip,
-        feelslike: response.body.current.feelslike
-      })
+      callback(undefined, 'Weather is ' + response.body.current.weather_descriptions[0] + '. Outside temperature is ' + response.body.current.temperature + ' celcius and feels like ' + response.body.current.feelslike + ' celcius, raining probability is %' + response.body.current.precip)
     }
   })
 }
