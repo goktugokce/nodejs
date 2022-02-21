1ee598ef51d0dff7ebfa1de2ea49861602fca794
goktugokce <goktugokce@windowslive.com>
2022-02-21 17:02:35 +0300

    json option setted

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,7 +2,6 @@ const request = require('request');
 
 const url = 'http://api.weatherstack.com/current?access_key=179e304ba8eae4b428944234999d878b&query=New York'
 
-request({url: url}, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.current)
+request({url: url, json:true}, (error, response) => {
+  console.log(response.body.current)
 });
