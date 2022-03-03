b2226863ada6fc523586d82bdac04bdd5d53992d
goktugokce <goktugokce@windowslive.com>
2022-03-03 09:17:33 +0300

    partials

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,47 +1,51 @@
-const express = require('express');
-const path = require('path');
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+
 const app = express();
 
-const viewsPath = path.join(__dirname,'../templates');
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
-app.set('view engine', 'hbs');
-app.set('views', viewsPath);
+// Setup handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
 
-app.use(express.static(path.join(__dirname, '../public')))
-
-app.get('', (req,res) => {
-  res.render('index',{
-    title:'Weather App',
-    name:'Goko'
-  })
-})
-
-app.get('/about', (req,res) => {
-  res.render('about', {
-    title:'About Me',
-    name:'Goktugokce'
-  })
-})
-
-app.get('/help', (req,res) => {
-  res.render('help', {
-    title:'Help',
-    name:'Goktugokce'
-  })
-})
-
-app.get('/weather', (req, res) => {
-  res.send({
-    forecast : 'It is snowing',
-    location : 'Philadelphia'
-  })
-})
-
-
-app.listen(3000, () => {
-  console.log('Server running on port 3000.');
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "goktugokce",
+  });
 });
 
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "goktugokce",
+  });
+});
 
+app.get("/help", (req, res) => {
+  res.render("help", {
+    helpText: "This is some helpful text.",
+    title: "Help",
+    name: "goktugokce",
+  });
+});
 
+app.get("/weather", (req, res) => {
+  res.send({
+    forecast: "It is snowing",
+    location: "Philadelphia",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is up on port 3000.");
+});
