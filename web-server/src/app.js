26a7f29f5b754c06a12c38ad5761a264408f518e
goktugokce <goktugokce@windowslive.com>
2022-03-03 08:58:01 +0300

    handlerbars templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,12 +1,35 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
 
+const viewsPath = path.join(__dirname,'../templates');
+
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
 
 
 app.use(express.static(path.join(__dirname, '../public')))
-app.use('/about', express.static(path.join(__dirname, '../public/about.html')))
 
+app.get('', (req,res) => {
+  res.render('index',{
+    title:'Weather App',
+    name:'Goko'
+  })
+})
+
+app.get('/about', (req,res) => {
+  res.render('about', {
+    title:'About Me',
+    name:'Goktugokce'
+  })
+})
+
+app.get('/help', (req,res) => {
+  res.render('help', {
+    title:'Help',
+    name:'Goktugokce'
+  })
+})
 
 app.get('/weather', (req, res) => {
   res.send({
@@ -20,8 +43,5 @@ app.listen(3000, () => {
   console.log('Server running on port 3000.');
 });
 
-//app.com
-//app.com/help
-//app.com/about
 
 
