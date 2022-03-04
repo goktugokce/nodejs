e16e8a928c0e223934f329ebbc4a0fedfbb07fe9
goktugokce <goktugokce@windowslive.com>
2022-03-04 10:35:54 +0300

    changed everything

---
+++ task-manager/mongodb.js
@@ -0,0 +1,16 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = 'mongodb://127.0.0.1:27017';
+
+//optional name
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, {useNewUrlParser:true}, (error, client) => {
+  if(error){
+    return console.log('Unable to connect to DB')
+  }
+
+  console.log('Connected correctly');
+
+})
