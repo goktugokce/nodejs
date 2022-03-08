e2ae8ded59422aae604d1e16b51d7a5d05eafb49
goktugokce <goktugokce@windowslive.com>
2022-03-08 15:55:52 +0300

    jwt-authentication

--- task-manager/src/routes/user.js
+++ task-manager/src/routes/user.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = new express.Router();
+const auth = require('../middleware/auth');
 const User = require("../models/user");
 
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
   try {
     await user.save();
-    res.status(201).send();
+    const token = await user.generateAuthToken()
+    res.status(201).send({user, token});
   } catch (e) {
     res.status(400).send(e);
   }
@@ -21,22 +23,21 @@ router.post("/users", async (req, res) => {
     });*/
 });
 
-router.post('/users/login', async (req,res) => {
-  try{
-    const user = await User.findByCredentials(req.body.email, req.body.password)
-    res.send(user)
-  }catch(e){
-    res.status(400).send()
-  }
-})
-
-router.get("/users", async (req, res) => {
+router.post("/users/login", async (req, res) => {
   try {
-    const users = await User.find({});
-    res.send(users);
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    const token = await user.generateAuthToken()
+    res.send({user, token});
   } catch (e) {
-    res.status(500);
+    res.status(400).send();
   }
+});
+
+router.get("/users/me", auth, async (req, res) => {
+  res.send(req.user)
 
   /*User.find({})
     .then((users) => {
