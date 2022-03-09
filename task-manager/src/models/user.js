2ea6c23d71f9efd7583b156eab0c845008fd93e4
goktugokce <goktugokce@windowslive.com>
2022-03-09 10:23:54 +0300

    finished dev part

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -1,116 +1,121 @@
-const mongoose = require("mongoose");
-const validator = require("validator");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken")
-const Task = require('./task');
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const Task = require('./task')
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error("Email is invalid");
-      }
+    name: {
+        type: String,
+        required: true,
+        trim: true
     },
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 7,
-    trim: true,
-    validate(value) {
-      if (value.toLowerCase().includes("password")) {
-        throw new Error('Password cannot contain "password"');
-      }
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
     },
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) {
-        throw new Error("Age must be a postive number");
-      }
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"')
+            }
+        }
     },
-  },
-  tokens:[{
-    token:{
-      type:String,
-      required:true,
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a postive number')
+            }
+        }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }],
+    avatar: {
+        type: Buffer
     }
-  }]
-});
-
-
-userSchema.virtual('tasks', {
-  ref:'Task',
-  localField:'_id',
-  foreignField:'owner'
+}, {
+    timestamps: true
 })
 
-userSchema.methods.generateAuthToken = async function() {
-  const user = this;
-  const token = jwt.sign({_id:user._id.toString()}, 'thisismynewcourse');
-  user.tokens = user.tokens.concat({token});
-  await user.save()
+userSchema.virtual('tasks', {
+    ref: 'Task',
+    localField: '_id',
+    foreignField: 'owner'
+})
 
-  return token;
+userSchema.methods.toJSON = function () {
+    const user = this
+    const userObject = user.toObject()
+
+    delete userObject.password
+    delete userObject.tokens
+    delete userObject.avatar
+
+    return userObject
 }
 
-userSchema.methods.toJSON = async function() {
-  const user = this;
-  const userObject = user.toObject();
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse')
 
-  delete userObject.password;
-  delete userObject.tokens;
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
 
-  return userObject;
+    return token
 }
 
 userSchema.statics.findByCredentials = async (email, password) => {
-  const user = await User.findOne({email})
+    const user = await User.findOne({ email })
 
-  if(!user){
-    throw new Error('Unable to login')
-  }
+    if (!user) {
+        throw new Error('Unable to login')
+    }
 
-  const isMatch = await bcrypt.compare(password, user.password)
-  if(!isMatch){
-    throw new Error('Unable to login')
-  }
+    const isMatch = await bcrypt.compare(password, user.password)
 
-  return user
+    if (!isMatch) {
+        throw new Error('Unable to login')
+    }
 
+    return user
 }
 
+// Hash the plain text password before saving
+userSchema.pre('save', async function (next) {
+    const user = this
 
-//hash the plain text password before saving
-userSchema.pre("save", async function (next) {
-  const user = this;
-  if (user.isModified("password")) {
-    user.password = await bcrypt.hash(user.password, 8);
-  }
-  next();
-});
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
 
-//delete user tasks when user is removed
-userSchema.pre('remove', async function(next){
-  const user = this;
-  
-  await Task.deleteMany({owner: user._id})
-  
-  next();
+    next()
 })
 
-const User = mongoose.model("User", userSchema);
+// Delete user tasks when user is removed
+userSchema.pre('remove', async function (next) {
+    const user = this
+    await Task.deleteMany({ owner: user._id })
+    next()
+})
 
-module.exports = User;
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
--- task-manager/src/routes/task.js
+++ task-manager/src/routes/task.js
@@ -1,34 +1,25 @@
-const express = require("express");
-const router = new express.Router();
-const Task = require("../models/task");
-const auth = require("../middleware/auth")
+const express = require('express')
+const Task = require('../models/task')
+const auth = require('../middleware/auth')
+const router = new express.Router()
 
-router.post("/tasks", auth, async (req, res) => {
-  //const task = new Task(req.body);
-
-  const task = new Task({
-    ...req.body,
-    owner:req.user._id
-  })
-
-  try {
-    await task.save();
-    res.status(201).send(task);
-  } catch (e) {
-    res.status(400).send();
-  }
-
-  /*
-  task
-    .save()
-    .then(() => {
-      res.status(201).send(task);
+router.post('/tasks', auth, async (req, res) => {
+    const task = new Task({
+        ...req.body,
+        owner: req.user._id
     })
-    .catch((e) => {
-      res.status(400).send(e);
-    });*/
-});
 
+    try {
+        await task.save()
+        res.status(201).send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+// GET /tasks?completed=true
+// GET /tasks?limit=10&skip=20
+// GET /tasks?sortBy=createdAt:desc
 router.get("/tasks", auth, async (req, res) => {
   try {
     const tasks = await Task.find({owner:req.user._id});
@@ -36,88 +27,60 @@ router.get("/tasks", auth, async (req, res) => {
   } catch (e) {
     res.status(500).send();
   }
+})
 
-  /*Task.find({})
-    .then((tasks) => {
-      res.send(tasks);
-    })
-    .catch((e) => {
-      res.status(500).send(e);
-    });*/
-});
+router.get('/tasks/:id', auth, async (req, res) => {
+    const _id = req.params.id
 
-router.get("/tasks/:id", auth, async (req, res) => {
-  const _id = req.params.id;
-  try {
-    const task = await Task.findOne({_id, owner:req.user._id})
-    if (!task) {
-      return res.status(404).send();
+    try {
+        const task = await Task.findOne({ _id, owner: req.user._id })
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (e) {
+        res.status(500).send()
     }
-    res.send(task);
-  } catch (e) {
-    res.send(500);
-  }
+})
 
-  /*Task.findById(_id)
-    .then((task) => {
-      if (!task) {
-        return res.status(404).send();
-      }
-      res.send(task);
-    })
-    .catch((e) => {
-      res.status(500).send(e);
-    });*/
-});
+router.patch('/tasks/:id', auth, async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
 
-router.patch("/tasks/:id", auth, async (req, res) => {
-  const updates = Object.keys(req.body);
-  const allowedUpdates = ["description", "completed"];
-  const isValidOperation = updates.every((update) =>
-    allowedUpdates.includes(update)
-  );
-
-  if (!isValidOperation) {
-    return res.status(400).send({ error: "Invalid Update Type" });
-  }
-
-  try {
-    const task = await Task.findOne({_id:req.params.id, owner:req.user._id});
-    
-    if (!task) {
-      return res.status(404).send();
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
     }
-    updates.forEach((update) => (task[update] = req.body[update]));
-    await task.save();
-    res.send(task);
-  } catch (e) {
-    res.status(404).send();
-  }
 
-  /*try {
-    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!task) {
-      return res.status(404).send();
+    try {
+        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id})
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        updates.forEach((update) => task[update] = req.body[update])
+        await task.save()
+        res.send(task)
+    } catch (e) {
+        res.status(400).send(e)
     }
-    res.send(task);
-  } catch (e) {
-    res.status(404).send();
-  }*/
-});
+})
 
-router.delete("/tasks/:id", auth, async (req, res) => {
-  try {
-    const task = await Task.findOneAndDelete({_id:req.params.id, owner:req.user._id});
-    if (!task) {
-      return res.status(404).send();
+router.delete('/tasks/:id', auth, async (req, res) => {
+    try {
+        const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id })
+
+        if (!task) {
+            res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (e) {
+        res.status(500).send()
     }
-    res.send(task);
-  } catch (e) {
-    res.status(404).send();
-  }
-});
+})
 
-module.exports = router;
+module.exports = router
