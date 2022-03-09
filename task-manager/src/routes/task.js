859b8fa49fca241aaf779c52b0680c09aea4465a
goktugokce <goktugokce@windowslive.com>
2022-03-09 10:54:50 +0300

    env variables

--- task-manager/src/routes/task.js
+++ task-manager/src/routes/task.js
@@ -30,57 +30,57 @@ router.get("/tasks", auth, async (req, res) => {
 })
 
 router.get('/tasks/:id', auth, async (req, res) => {
-    const _id = req.params.id
+  const _id = req.params.id
 
-    try {
-        const task = await Task.findOne({ _id, owner: req.user._id })
+  try {
+      const task = await Task.findOne({ _id, owner: req.user._id })
 
-        if (!task) {
-            return res.status(404).send()
-        }
+      if (!task) {
+          return res.status(404).send()
+      }
 
-        res.send(task)
-    } catch (e) {
-        res.status(500).send()
-    }
+      res.send(task)
+  } catch (e) {
+      res.status(500).send()
+  }
 })
 
 router.patch('/tasks/:id', auth, async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['description', 'completed']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+  const updates = Object.keys(req.body)
+  const allowedUpdates = ['description', 'completed']
+  const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
 
-    if (!isValidOperation) {
-        return res.status(400).send({ error: 'Invalid updates!' })
-    }
+  if (!isValidOperation) {
+      return res.status(400).send({ error: 'Invalid updates!' })
+  }
 
-    try {
-        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id})
+  try {
+      const task = await Task.findOne({ _id: req.params.id, owner: req.user._id})
 
-        if (!task) {
-            return res.status(404).send()
-        }
+      if (!task) {
+          return res.status(404).send()
+      }
 
-        updates.forEach((update) => task[update] = req.body[update])
-        await task.save()
-        res.send(task)
-    } catch (e) {
-        res.status(400).send(e)
-    }
+      updates.forEach((update) => task[update] = req.body[update])
+      await task.save()
+      res.send(task)
+  } catch (e) {
+      res.status(400).send(e)
+  }
 })
 
 router.delete('/tasks/:id', auth, async (req, res) => {
-    try {
-        const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id })
+  try {
+      const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id })
 
-        if (!task) {
-            res.status(404).send()
-        }
+      if (!task) {
+          res.status(404).send()
+      }
 
-        res.send(task)
-    } catch (e) {
-        res.status(500).send()
-    }
+      res.send(task)
+  } catch (e) {
+      res.status(500).send()
+  }
 })
 
 module.exports = router
--- task-manager/src/routes/user.js
+++ task-manager/src/routes/user.js
@@ -1,14 +1,17 @@
 const express = require("express");
+const multer = require("multer");
+const sharp = require("sharp");
 const User = require("../models/user");
 const auth = require("../middleware/auth");
+const { sendWelcomeEmail, sendCancelationEmail } = require("../emails/account");
 const router = new express.Router();
-const multer = require('multer')
 
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
 
   try {
     await user.save();
+    sendWelcomeEmail(user.email, user.name);
     const token = await user.generateAuthToken();
     res.status(201).send({ user, token });
   } catch (e) {
@@ -79,6 +82,7 @@ router.patch("/users/me", auth, async (req, res) => {
 router.delete("/users/me", auth, async (req, res) => {
   try {
     await req.user.remove();
+    sendCancelationEmail(req.user.email, req.user.name);
     res.send(req.user);
   } catch (e) {
     res.status(500).send();
@@ -87,45 +91,54 @@ router.delete("/users/me", auth, async (req, res) => {
 
 const upload = multer({
   limits: {
-      fileSize: 1000000
+    fileSize: 1000000,
   },
   fileFilter(req, file, cb) {
-      if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-          return cb(new Error('Please upload an image'))
-      }
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error("Please upload an image"));
+    }
 
-      cb(undefined, true)
+    cb(undefined, true);
+  },
+});
+
+router.post(
+  "/users/me/avatar",
+  auth,
+  upload.single("avatar"),
+  async (req, res) => {
+    const buffer = await sharp(req.file.buffer)
+      .resize({ width: 250, height: 250 })
+      .png()
+      .toBuffer();
+    req.user.avatar = buffer;
+    await req.user.save();
+    res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
   }
-})
+);
 
-router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-  const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
-  req.user.avatar = buffer
-  await req.user.save()
-  res.send()
-}, (error, req, res, next) => {
-  res.status(400).send({ error: error.message })
-})
+router.delete("/users/me/avatar", auth, async (req, res) => {
+  req.user.avatar = undefined;
+  await req.user.save();
+  res.send();
+});
 
-router.delete('/users/me/avatar', auth, async (req, res) => {
-  req.user.avatar = undefined
-  await req.user.save()
-  res.send()
-})
-
-router.get('/users/:id/avatar', async (req, res) => {
+router.get("/users/:id/avatar", async (req, res) => {
   try {
-      const user = await User.findById(req.params.id)
+    const user = await User.findById(req.params.id);
 
-      if (!user || !user.avatar) {
-          throw new Error()
-      }
+    if (!user || !user.avatar) {
+      throw new Error();
+    }
 
-      res.set('Content-Type', 'image/png')
-      res.send(user.avatar)
+    res.set("Content-Type", "image/png");
+    res.send(user.avatar);
   } catch (e) {
-      res.status(404).send()
+    res.status(404).send();
   }
-})
+});
 
 module.exports = router;
